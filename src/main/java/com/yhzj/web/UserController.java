df084e82d219534fe632f06791f4fd2265cd9e9d
qinling <1464100250@qq.com>
2018-05-30 14:45:34 +0800

    修复了exception的bug；并添加的通过id查询用户方法

--- src/main/java/com/yhzj/web/UserController.java
+++ src/main/java/com/yhzj/web/UserController.java
@@ -30,7 +30,6 @@ public class UserController {
     public Result getAll() {
         return ResultUtil.success(userService.getAll());
     }
-
     @GetMapping("findById/{id}")
     public Result getById(@PathVariable("id")Integer id) {
         return ResultUtil.success(userService.getById(id));
