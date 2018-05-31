114bf45dc5de6a0caca49467ace7c9fe8c57a625
root <852329742@qq.com>
2018-05-31 16:35:01 +0800

    查询用户记录数

--- src/main/java/com/yhzj/mapper/UserMapper.java
+++ src/main/java/com/yhzj/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.yhzj.mapper;
 
 import com.yhzj.entity.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -23,4 +24,15 @@ public interface UserMapper {
     void upDataUser(User user);
     //更新账户表
     void upDataAccount(User user);
+
+    /**
+     * 查询用户记录数
+     * @return
+     */
+    public int count();
+    //查询用户列表
+    public List<User> getUserList(@Param("userName")String userName,
+                                  @Param("userRole")Integer roleId,
+                                  @Param("from")Integer currentPageNo,
+                                  @Param("pageSize")Integer pageSize);
 }
